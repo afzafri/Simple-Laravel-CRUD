1dfc54eab75039af7a3c1293b50ff56d462130c0
Afif Zafri <zafri_21596@yahoo.com>
2016-10-31 17:07:08 +0800

    Fixed error load JS file and mobile view

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,5 +1,30 @@
 <title>Simple CRUD Laravel</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('bootstrap/js/bootstrap.min.js') }}">
-<link rel="stylesheet" href="{{ asset('js/jquery.js') }}">
 <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+
+<script>
+
+	$(document).ready(function(){
+	  	// Remove active for all items.
+		  $('.navbar-nav li').removeClass('active');
+
+		  
+		  // highlight submenu item
+		  var locations = this.location.pathname;
+		  //var newloc = "." + locations.substring(8);
+		  $('li a[href="' + locations + '"]').parent().addClass('active');
+		  //console.log(locations);
+
+		  if(locations == "/")
+		  {
+		    $('li a[href="/"]').parent().addClass('active');
+		  }
+	 });
+	
+	
+</script>
