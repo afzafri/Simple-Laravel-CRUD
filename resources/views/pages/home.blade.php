cce9628b2cc438445bd75eddcb94c393257f8e6e
Afif Zafri <zafri_21596@yahoo.com>
2016-10-31 18:30:54 +0800

    add image upload input field

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,8 +4,9 @@
 
 <div class="panel panel-success">
 		<div class="panel-heading">Insert Stock</div>
-		<form action="/" method="post">
+		<form action="/" method="post" enctype="multipart/form-data">
 		<div class="panel-body">
+
 			<label class="label-control">Stock Type</label> 
 			<select name="stype" class="form-control" required="required">
 				<option value=''>Please choose stock type</option>
@@ -15,26 +16,41 @@
 				<option value='Formal Shirt'>Formal Shirt</option>
 			</select>
 			<br>
+
 			<label class="label-control">Stock Name/Description</label> 
 			<input type="text" name="sname" class="form-control" placeholder="Please input stock name/description" required="required">
 			<br>
-			<div class="col-md-3">
-			<label class="label-control">Stock Size</label> 
-			<select name="ssize" class="form-control" required="required">
-				<option value=''>Please choose stock size</option>
-				<option value='S'>S</option>
-				<option value='M'>M</option>
-				<option value='L'>L</option>
-				<option value='XL'>XL</option>
-			</select>
-			</div>
-			
 
-			<div class="col-md-2">
-			<label class="label-control">Stock Quantity</label> 
-			<input type="number" name="squantity" class="form-control" required="required" placeholder="Insert quantity" value="0">
+			<div class="row">
+				<div class="col-md-3">
+					<label class="label-control">Stock Size</label> 
+					<select name="ssize" class="form-control" required="required">
+						<option value=''>Please choose stock size</option>
+						<option value='S'>S</option>
+						<option value='M'>M</option>
+						<option value='L'>L</option>
+						<option value='XL'>XL</option>
+					</select>
+				</div>
+				
+
+				<div class="col-md-2">
+					<label class="label-control">Stock Quantity</label> 
+					<input type="number" name="squantity" class="form-control" required="required" placeholder="Insert quantity" value="0">
+				</div>
 			</div>
 			<br>
+
+			
+			<div class="row">
+				<div class="col-md-5">
+					<label class="label-control">Upload Photo</label> 
+					<input type="file" name="fileUpload" class="form-control" required="required">
+					<i>Note: Only jpg,jpeg file allowed. Max size: 3MB</i>
+				</div>
+			</div>
+			<br>
+
 			<input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
 		
 	</div>
