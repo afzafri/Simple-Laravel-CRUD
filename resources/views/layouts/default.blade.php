fd713038828793f6ba59d295ee16d6d7f5c07fa0
Afif Zafri <zafri_21596@yahoo.com>
2016-10-31 19:08:09 +0800

    add loading animation

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -16,5 +16,17 @@
 <div class="footer" style="text-align:center">
 Simple CRUD Web App using Laravel Framework. Dev by Afif Zafri.
 </div>
+
+<div class="overlay">
+  <div class="popup">
+		<div class="sk-folding-cube">
+		  <div class="sk-cube1 sk-cube"></div>
+		  <div class="sk-cube2 sk-cube"></div>
+		  <div class="sk-cube4 sk-cube"></div>
+		  <div class="sk-cube3 sk-cube"></div>
+		</div>
+	</div>
+</div>
+	
 </body>
 </html>
