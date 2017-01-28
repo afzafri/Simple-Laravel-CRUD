c51f7cdf80efcb99749fead12c075b7e563501c5
Afif Zafri <afzafri@gmail.com>
2017-01-29 00:53:23 +0800

    update Stock model

--- app/Stock.php
+++ app/Stock.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    //table name
-    protected $table = 'stocks';
-    protected $primaryKey = 'STK_ID';
-    public $timestamps = false;
     
 }
