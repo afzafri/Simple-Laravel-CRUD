d788837e0ab6d90b8e412d6ef6366034a67f9b87
Afif Zafri <afzafri@gmail.com>
2017-01-29 22:24:15 +0800

    remove CSRF token for search form, and remove submit button name

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -9,11 +9,10 @@
 			<label class="label-control">Stock Name</label> 
 			<input type="text" name="sname" class="form-control" placeholder="Please input stock name/description" required="required">
 			<br>
-			<input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
 		
 	</div>
 	<div class="panel-footer">
-		<button type="submit" name="submit" class="btn btn-success">Search</button>
+		<button type="submit" class="btn btn-success">Search</button>
 	</div>
 	</form>
 </div>
