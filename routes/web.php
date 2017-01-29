017e1d8825f23170f1146b80e60dd02ff90e1641
Afif Zafri <afzafri@gmail.com>
2017-01-29 22:10:50 +0800

    update routes

--- routes/web.php
+++ routes/web.php
@@ -20,20 +20,23 @@ Route::get('/home', 'ProcessController@homepage');
 
 //PROCESS CONTROLLER
 //insert data
-Route::post('/home',array('uses'=>'ProcessController@store'));
+Route::post('/home', 'ProcessController@store');
 
 //delete data
-Route::post('/view',array('uses'=>'ProcessController@destroy'));
+Route::post('/view', 'ProcessController@destroy');
 
 //List all data 
-Route::get('/view',array('uses'=>'ProcessController@index'));
+Route::get('/view', 'ProcessController@index');
 
 //search data
-Route::get('/search',array('uses'=>'ProcessController@show'));
+Route::get('/search', 'ProcessController@show');
 
-Route::get('/edit/{id}',array('uses'=>'ProcessController@edit'));
+// edit and update data
+Route::get('/edit/{id}', 'ProcessController@edit');
 
-Route::post('/update',array('uses'=>'ProcessController@update'));
+Route::post('/update', 'ProcessController@update');
+
+// Auth routes, login, register, forgot pass
 Auth::routes();
 
 
