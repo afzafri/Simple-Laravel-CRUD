daecc10b10515b29c85c6f2f2f1e94bf9f1c16af
Afif Zafri <zafri_21596@yahoo.com>
2018-08-26 15:08:16 +0800

    Pass delete stock confirm message to the function

--- resources/views/pages/view.blade.php
+++ resources/views/pages/view.blade.php
@@ -4,11 +4,11 @@
 
 <div class="panel panel-success">
 		<div class="panel-heading">List Stock</div>
-		
+
 		<div class="panel-body">
 
 		<div class='table-responsive'>
-		<form action="/view" method="post" onsubmit="return showLoad()">
+		<form action="/view" method="post" onsubmit="return showLoad('Delete stock?')">
 		  <table class='table table-bordered table-hover'>
 		    <thead>
 		      <tr>
@@ -23,7 +23,7 @@
 		    </thead>
 		    <tbody>
 			<!-- iterate through the array of the stocks to display them -->
-			@foreach($liststock as $liststocks) 
+			@foreach($liststock as $liststocks)
 				<tr>
 					<td>{{$liststocks->id}}</td>
 					<td>{{$liststocks->stk_type}}</td>
@@ -31,15 +31,15 @@
 					<td><img src='{{asset("/images/$liststocks->id.jpg")}}' width="150px" height="150px" class="img-thumbnail img-responsive" title="{{$liststocks->stk_name.' '.$liststocks->stk_type}}"/></td>
 					<td>{{$liststocks->stk_size}}</td>
 					<td>{{$liststocks->stk_qty}}</td>
-					
-					
+
+
 					<td align="center"><a href='/edit/{{$liststocks->id}}' data-toggle="tooltip" title="Update Stock" class='btn btn-success' onclick='return confirm("Edit stock?");'><i class='fa fa-fw fa-gear'></i></a>
-					<button type='submit' class='btn btn-danger' onclick='return confirm("Delete stock?");' data-toggle="tooltip" title="Delete Stock"><i class='fa fa-fw fa-trash'></i></button></td>
+					<button type='submit' class='btn btn-danger' data-toggle="tooltip" title="Delete Stock"><i class='fa fa-fw fa-trash'></i></button></td>
 					<td style='display:none;'><input type='text' name='delstock' value='{{$liststocks->id}}' style='display:none;'></td>
 					{{ csrf_field() }}
 				</tr>
 			@endforeach
-				
+
 			</tbody>
 				</table>
 				</form>
