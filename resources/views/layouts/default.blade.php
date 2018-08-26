eebd80ec250e3e702900806f0abc53affb8d14d1
Afif Zafri <zafri_21596@yahoo.com>
2018-08-26 14:45:50 +0800

    Move scripts to the bottom

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,39 +9,6 @@
 	<link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/design.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/fileinput.min.css') }}">
-	<script src="{{asset('js/jquery.js')}}"></script>
-	<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('js/fileinput.min.js')}}"></script>
-
-	<script>
-
-		$(document).ready(function(){
-
-			//hide loading animation
-			$('.overlay').hide();
-	     	$('.sk-folding-cube').hide();
-
-	     	// Remove active for all items.
-			$('.navbar-nav li').removeClass('active');
-
-			var url = window.location;
-
-			// for sidebar menu entirely but not cover treeview
-			$('ul.navbar-nav a').filter(function() {
-			  return this.href == url;
-			}).parent().addClass('active');
-
-		 });
-
-		// when called, will show loading animation
-		function showLoad($msg)
-		{
-			return confirm($msg);
-			$('.overlay').show();
-	  	$('.sk-folding-cube').show();
-		}
-
-	</script>
 </head>
 <body>
 
@@ -105,6 +72,41 @@ Simple CRUD Web App using Laravel Framework. Dev by Afif Zafri.
 		</div>
 	</div>
 </div>
+
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="{{asset('bootstrap/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/fileinput.min.js')}}"></script>
+
+<script>
+
+	$(document).ready(function(){
+
+		//hide loading animation
+		$('.overlay').hide();
+			$('.sk-folding-cube').hide();
+
+			// Remove active for all items.
+		$('.navbar-nav li').removeClass('active');
+
+		var url = window.location;
+
+		// for sidebar menu entirely but not cover treeview
+		$('ul.navbar-nav a').filter(function() {
+			return this.href == url;
+		}).parent().addClass('active');
+
+	 });
+
+	// when called, will show loading animation
+	function showLoad($msg)
+	{
+		if($msg != null)
+			return confirm($msg);
+		$('.overlay').show();
+		$('.sk-folding-cube').show();
+	}
+
+</script>
 
 </body>
 </html>
